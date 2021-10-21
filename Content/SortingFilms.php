14ed102a3948ae5a74a8c53d82abab47a4e0aa03
ПрохоровВлад <vlad_maik7912@vk.com>
2021-10-21 11:32:06 +0300

    SortingFilms.php - исправил на читабельный вид

--- Content/SortingFilms.php
+++ Content/SortingFilms.php
@@ -3,24 +3,31 @@
 /**@var array $movies */
 require 'movies.php';
 
-$i=1;
+$i = 1;
 
 echo "Ведите ваш возраст: ";
 $age = readline();
 
-if (is_numeric($age)) {
-	echo "Ваш возраст: $age+ " . "\n"."Вам доступны следующие фильмы c возрастным рейтингом $age+ и ниже:" . "\n";
-} else {
-	echo "Ведите только ваш возраст (цаифрами)"; exit();
+if (is_numeric($age))
+{
+	echo "Ваш возраст: $age+ " . "\n" . "Вам доступны следующие фильмы c возрастным рейтингом $age+ и ниже:" . "\n";
+}
+else
+{
+	echo "Ведите только ваш возраст (цаифрами)";
+	exit();
 }
 
-function formatMovie (array $movie): string {
+function formatMovie(array $movie): string
+{
 	return ". {$movie['title']} ({$movie['release_year']}), {$movie['age_restriction']}+. Rating - {$movie['rating']} \n";
 }
 
-foreach($movies as $movie) {
-	if($movie["age_restriction"] <= (int)$age) {
-		echo $i++.formatMovie($movie);
+foreach ($movies as $movie)
+{
+	if ($movie["age_restriction"] <= (int)$age)
+	{
+		echo $i++ . formatMovie($movie);
 	}
 }
 
